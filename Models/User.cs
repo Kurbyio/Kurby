414ed6d8d60d1382c856741b5f48d70b9e6d9c76
Dan Rovito <drovito@thinkwarenet.com>
2020-01-03 16:24:00 -0500

    User Model Namespace

--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,18 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-public class User
+namespace vancil.Models
 {
-    [Key]
-    public int Id {get; set;}
-    public string Name {get;set;}
-    [Required]
-    public string Email {get;set;}
-    [Required]
-    public string Password{get;set;}
-    public DateTime CreatedAt {get;set;} = DateTime.Now;
-    public DateTime UpdatedAt{get;set;}
+    public class User
+    {
+        [Key]
+        public int Id {get; set;}
+        public string Name {get;set;}
+        [Required]
+        public string Email {get;set;}
+        [Required]
+        public string Password{get;set;}
+        public DateTime CreatedAt {get;set;} = DateTime.Now;
+        public DateTime UpdatedAt{get;set;}
+    }
 }
