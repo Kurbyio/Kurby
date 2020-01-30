44be8a8e17a7c2c3848d97c5cb1d02e25df46b73
Dan Rovito <danrovito@gmail.com>
2020-01-30 17:05:51 -0500

    added Test'

---
+++ Kurby.Test/Data/InMemory.cs
@@ -0,0 +1,21 @@
+﻿using Kurby.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kurby.Test.Data
+{
+    class InMemory
+    {
+        public DbContext dbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                            .Options;
+            var context = new DbContext(options);
+
+            return context;
+        }
+    }
+}
